c68cd32f2b0d78d86a5d18ce8ab54d9b5484e7c8
laurhakob <laurhakob@gmail.com>
2025-03-19 00:50:53 +0400

    project structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center ">
-      <p>Hello world</p>
-      <Button>Click</Button>
+      Click
+      <Link href="/documents/123">
+        <span className="text-blue-500 underline">here</span>
+      </Link>
+      to go to docu,ent id
     </div>
   );
 };
