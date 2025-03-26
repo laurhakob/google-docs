74997a2faa54bc8206e5cec48330c37d8f07030d
laurhakob <laurhakob@gmail.com>
2025-03-26 04:37:16 +0400

    Auth setup(clerk)

---
+++ convex/auth.config.ts
@@ -0,0 +1,22 @@
+export default {
+  providers: [
+    {
+      domain: "https://great-lemur-1.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+//chat gbt
+
+// const clerkConfig = {
+//     providers: [
+//       {
+//         domain: "https://great-lemur-1.clerk.accounts.dev",
+//         applicationID: "convex",
+//       },
+//     ],
+//   };
+  
+//   export default clerkConfig;
+  
